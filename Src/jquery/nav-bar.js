c94ced579d2f12ae868cca535bc7dc43f25ae419
Miguel Henrique <miguelhp373@gmail.com>
2021-03-21 16:01:56 -0300

    Update - mobile - version1

--- Src/jquery/nav-bar.js
+++ Src/jquery/nav-bar.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
         if ($(this).scrollTop() > 10) {
             $('.nav-bar-top-fixed-home').css({'background-color':'#ecf0f1'});
             $('.links-item-nav').attr('style', 'color: black !important')
-            $('a').attr('style', 'color: black !important')
+            $('.items-nav-bar').attr('style', 'color: black !important')
             $('.nav-bar-top-fixed-home').css("opacity","90%");
            
         } else {
             $('.nav-bar-top-fixed-home').css("background-color", "transparent")
             $('.nav-bar-top-fixed-home').css("opacity","100%");
             $('.links-item-nav').attr('style', 'color: #FFF !important')
-            $('a').attr('style', 'color: #FFF !important')
+            $('.items-nav-bar').attr('style', 'color: #FFF !important')
             $('.dropdown-item').attr('style', 'color: #000 !important')
         }
     });

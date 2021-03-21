bf1051e728371b06352838e480dce4f69d9bf2e0
Miguel Henrique <miguelhp373@gmail.com>
2021-03-20 22:50:53 -0300

    Versão 1.0 - Remodução do Site Original

---
+++ Src/jquery/nav-bar.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.nav-bar-top-fixed-home').css({'background-color':'#ecf0f1'});
+            $('.links-item-nav').attr('style', 'color: black !important')
+            $('a').attr('style', 'color: black !important')
+            $('.nav-bar-top-fixed-home').css("opacity","90%");
+           
+        } else {
+            $('.nav-bar-top-fixed-home').css("background-color", "transparent")
+            $('.nav-bar-top-fixed-home').css("opacity","100%");
+            $('.links-item-nav').attr('style', 'color: #FFF !important')
+            $('a').attr('style', 'color: #FFF !important')
+            $('.dropdown-item').attr('style', 'color: #000 !important')
+        }
+    });
+});
